3220b501151100591e30bc1aba02232bbc29d129
erandro <erandro@gmail.com>
2019-05-17 09:45:19 -0500

    added logic for printing on the html and getting unique status from the JSON

--- JSON-formatted-data.js
+++ JSON-formatted-data.js
@@ -1,6 +1,29 @@
+function appendInfoOnHtml() {
+    $("#content").append(`<div id="first"><p>The data is from this <a href="http://it-recruitment.mintel.com/testing/test_data.json" target="_blank">JSON</a>.</p></div>`)
+    $("#content").append(`<div id="second"><p>There are ${uniqueStatuses} unique statuses in this JSON.</p></div>`)
+}
+
+let uniqueStatuses;
+
 $.ajax({
     url: "http://localhost:8080/it-recruitment.mintel.com/testing/test_data.json",
     method: "GET"
 }).then(function (data) {
-    console.log(data);
+    uniqueStatuses = getUniqueStatus(data);
+    appendInfoOnHtml()
 });
+
+function getUniqueStatus(data) {
+    let statusObj = {}
+    let uniqueStatusObj = {}
+    for (let i = 0; i < data.length; i++) {
+        if (statusObj[data[i].status]) {
+            delete uniqueStatusObj[data[i].status];
+        } else {
+            statusObj[data[i].status] = data[i].status;
+            uniqueStatusObj[data[i].status] = data[i].status;
+        }
+    }
+    return Object.keys(uniqueStatusObj).length;
+}
+
